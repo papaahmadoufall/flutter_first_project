6b488afe6d639d39ff22e5685f1c76d84b52590f
papaahmadoufall <pahmadoudou@gmail.com>
2023-07-14 11:52:58 +0000

    adding second constructor method for gradientWidget, adding image and button wrapped in a column

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:first_project/styled_text.dart';
+// import 'package:first_project/styled_text.dart';
 
 const startAlignement = Alignment.topLeft;
-const endAlignement =  Alignment.bottomRight;
+const endAlignement = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer( this.colors, {super.key });
-  
+  const GradientContainer(this.colors, {super.key});
+
   final List<Color> colors;
-  GradientContainer.gradient({super.key}): colors = [Colors.blue, Colors.blueGrey]  ;
- 
-  
+  GradientContainer.gradient({super.key})
+      : colors = [Colors.blue, Colors.blueGrey];
+
+  void rollDice() {}
 
   @override
-  Widget build(context){
-      return  Container(
-          decoration:  BoxDecoration(
-            gradient: LinearGradient(
-               colors: colors,
-              begin: startAlignement,
-              end: endAlignement,
-            ),
+  Widget build(context) {
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colors,
+            begin: startAlignement,
+            end: endAlignement,
           ),
-          child: Center(
-            child: Image.asset("assets/images/dice-1.png", width: 200,),
-            ),
-          );
-       
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/dice-1.png",
+                width: 200,
+              ),
+               TextButton(onPressed: rollDice, 
+               child: const Text("roll dice",style: TextStyle(color: Colors.amber)),)
+
+            ],
+          ),
+        ));
   }
 }
